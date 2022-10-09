b5c53034d066ad1a3bf9c6d271ce2e34e7ef8a96
meremeev <meremeev@sfedu.ru>
2022-10-10 01:07:06 +0300

    feat(Module7): implement AssignCountVisitor

--- Module7/Visitors/AssignCountVisitor.cs
+++ Module7/Visitors/AssignCountVisitor.cs
@@ -8,7 +8,25 @@ namespace SimpleLang.Visitors
 {
     public class AssignCountVisitor : AutoVisitor
     {
-        public int Count = 0;
-        
+        private int _count = 0;
+        public int Count => _count;
+
+        public override void VisitAssignNode(AssignNode a)
+        {
+            _count += 1;
+        }
+
+        public override void VisitWriteNode(WriteNode w)
+        {
+        }
+
+        public override void VisitVarDefNode(VarDefNode w)
+        {
+        }
+
+        public void Reset()
+        {
+            _count = 0;
+        }
     }
 }
